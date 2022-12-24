d09201bfe7005350434a7fef849f5c49699c0133
Chris Brower <chris@devpad.org>
2022-12-23 23:21:56 -0600

    Standardize CLI flags on dashes

--- cmd/library/library_create/library_create.go
+++ cmd/library/library_create/library_create.go
@@ -42,7 +42,7 @@ func NewLibraryCreateCommand() *cobra.Command {
 		Short: "Create a new library",
 		PreRun: func(cmd *cobra.Command, args []string) {
 			_ = viper.BindPFlag("server.dbLog", cmd.Flags().Lookup("db-log"))
-			_ = viper.BindPFlag("server.sqliteDir", cmd.Flags().Lookup("sqlite_dir"))
+			_ = viper.BindPFlag("server.sqliteDir", cmd.Flags().Lookup("sqlite-dir"))
 			_ = viper.BindPFlag("database.connection", cmd.Flags().Lookup("db-conn"))
 			_ = viper.BindPFlag("metadata.scanHidden", cmd.Flags().Lookup("scan-hidden"))
 		},
@@ -67,9 +67,9 @@ func NewLibraryCreateCommand() *cobra.Command {
 	c.Flags().StringVar(&filePath, "path", "", "Path for this library")
 	c.MarkFlagRequired("path")
 
-	c.Flags().IntVar(&mediaType, "media_type", 0, "Media type, 0 for Movies, 1 for Series")
-	c.Flags().IntVar(&backendType, "backend_type", 0, "Backend type, 0 for Local, 1 for Rclone")
-	c.Flags().StringVar(&rcloneName, "rclone_name", "", "Name for the Rclone remote")
+	c.Flags().IntVar(&mediaType, "media-type", 0, "Media type, 0 for Movies, 1 for Series")
+	c.Flags().IntVar(&backendType, "backend-type", 0, "Backend type, 0 for Local, 1 for Rclone")
+	c.Flags().StringVar(&rcloneName, "rclone-name", "", "Name for the Rclone remote")
 
 	c.Flags().Bool("db-log", false, "sets whether the database should log queries")
 	c.Flags().String("db-conn", "", "sets the database connection string")
--- cmd/root/root.go
+++ cmd/root/root.go
@@ -19,13 +19,13 @@ func NewRootCommand() *cobra.Command {
 	c := &cobra.Command{
 		Use: "olaris",
 		PersistentPreRun: func(cmd *cobra.Command, args []string) {
-			_ = viper.BindPFlag("config.dir", cmd.Flags().Lookup("config_dir"))
-			_ = viper.BindPFlag("server.cacheDir", cmd.Flags().Lookup("cache_dir"))
+			_ = viper.BindPFlag("config.dir", cmd.Flags().Lookup("config-dir"))
+			_ = viper.BindPFlag("server.cacheDir", cmd.Flags().Lookup("cache-dir"))
 			_ = viper.BindPFlag("server.verbose", cmd.Flags().Lookup("verbose"))
-			_ = viper.BindPFlag("server.directFileAccess", cmd.Flags().Lookup("allow_direct_file_access"))
-			_ = viper.BindPFlag("debug.streamingPages", cmd.Flags().Lookup("enable_streaming_debug_pages"))
-			_ = viper.BindPFlag("debug.transcoderLog", cmd.Flags().Lookup("write_transcoder_log"))
-			_ = viper.BindPFlag("rclone.configFile", cmd.Flags().Lookup("rclone_config"))
+			_ = viper.BindPFlag("server.directFileAccess", cmd.Flags().Lookup("allow-direct-file-access"))
+			_ = viper.BindPFlag("debug.streamingPages", cmd.Flags().Lookup("enable-streaming-debug-pages"))
+			_ = viper.BindPFlag("debug.transcoderLog", cmd.Flags().Lookup("write-transcoder-log"))
+			_ = viper.BindPFlag("rclone.configFile", cmd.Flags().Lookup("rclone-config"))
 
 			// Configure Viper
 			viper.SetConfigName("olaris")
@@ -36,13 +36,13 @@ func NewRootCommand() *cobra.Command {
 		},
 	}
 
-	c.PersistentFlags().Bool("allow_direct_file_access", false, "Whether accessing files directly by path (without a valid JWT) is allowed")
-	c.PersistentFlags().Bool("enable_streaming_debug_pages", false, "Whether to enable debug pages in the streaming server")
-	c.PersistentFlags().Bool("write_transcoder_log", true, "Whether to write transcoder output to logfile")
+	c.PersistentFlags().Bool("allow-direct-file-access", false, "Whether accessing files directly by path (without a valid JWT) is allowed")
+	c.PersistentFlags().Bool("enable-streaming-debug-pages", false, "Whether to enable debug pages in the streaming server")
+	c.PersistentFlags().Bool("write-transcoder-log", true, "Whether to write transcoder output to logfile")
 	c.PersistentFlags().BoolP("verbose", "v", true, "verbose logging")
-	c.PersistentFlags().String("config_dir", config.GetDefaultConfigDir(), "Default configuration directory for config files")
-	c.PersistentFlags().String("rclone_config", helpers.GetDefaultRcloneConfigPath(), "Default rclone configuration file")
-	c.PersistentFlags().String("cache_dir", helpers.GetDefaultCacheDir(), "Cache directory for transcoding an other temporarily files")
+	c.PersistentFlags().String("config-dir", config.GetDefaultConfigDir(), "Default configuration directory for config files")
+	c.PersistentFlags().String("rclone-config", helpers.GetDefaultRcloneConfigPath(), "Default rclone configuration file")
+	c.PersistentFlags().String("cache-dir", helpers.GetDefaultCacheDir(), "Cache directory for transcoding an other temporarily files")
 
 	return c
 }
